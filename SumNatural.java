dd8f0733dd0601714d4ba2c18126129c846f3e74
Anuj Kr. Jha <96413803+its-jhaanuj-21@users.noreply.github.com>
2022-09-23 20:51:00 +0530

    Add files via upload

---
+++ SumNatural.java
@@ -0,0 +1,9 @@
+class SumNatural {
+    public static void main(String[] args) {
+        int i, sum = 0;
+        for(i=1; i<=50; i++){
+            sum += i;
+        }
+        System.out.println("Sum of all natural number from 1 to 50: "+sum);
+    }
+}
